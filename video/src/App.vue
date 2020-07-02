7e9d2c02ef83d8a5a4b86f97dae759eaaec846b0
Shubham Shahi <48720883+shubhamshahi@users.noreply.github.com>
2020-07-02 10:26:22 +0530

    Add files via upload

---
+++ video/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+
+  <div id="app">
+  
+    <vue-webrtc ref="webrtc" width="100%" roomId="sample-room"/>
+    
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import WebRTC from 'vue-webrtc'
+
+// ISSUE 5: https://github.com/westonsoftware/vue-webrtc/issues/5
+import * as io from 'socket.io-client'
+window.io = io
+//
+
+Vue.use(WebRTC)
+
+export default {
+  name: 'App',
+  mounted: function () {
+    this.$refs.webrtc.join()
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
